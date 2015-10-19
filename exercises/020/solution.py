abbd92df05c48653d54be9d8e9ba2baeb9a78b47
fporte <franck.porteous@cri-paris.org>
2015-10-19 11:21:51 +0200

    int float liste exist sys.arv

--- exercises/020/solution.py
+++ exercises/020/solution.py
@@ -1,17 +1,9 @@
 #! /usr/bin/env python
-
 import sys
-
 if len(sys.argv) > 1:
-
     a = int(sys.argv[1])
     b = int(sys.argv[2])
-
     r = a - b
-
     print(r)   
-	
 else:
-
-
     print("usage: python3 solution.py OP1 OP2")
