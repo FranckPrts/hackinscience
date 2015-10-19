563a15ceaa1ae446be5bd0853e42313f28e6ca86
fporte <franck.porteous@cri-paris.org>
2015-10-19 11:30:49 +0200

    int float liste exist sys.arv sans les espaces

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -3,7 +3,7 @@ import sys
 if len(sys.argv) > 1:
     a = int(sys.argv[1])
     b = int(sys.argv[2])
-    r = a - b
+    r = a + b
     print(r)
 else:
     print("usage: python3 solution.py OP1 OP2")
