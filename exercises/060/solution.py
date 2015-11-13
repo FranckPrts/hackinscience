d1d95eee7d906302af8f698a6a1a46cd71b1a30e
fporte <franck.porteous@cri-paris.org>
2015-11-13 15:37:11 +0100

    afficher/ligne tout les combi aa&ab&ac...

---
+++ exercises/060/solution.py
@@ -0,0 +1,7 @@
+a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+for i in a:
+    for j in a:
+        if i == "z" and j == "z":
+            a = a
+        print(i + j)
