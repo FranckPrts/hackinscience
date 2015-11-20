1e796ea598da28216d797e6ddfd088d717e369be
fporte <franck.porteous@cri-paris.org>
2015-11-20 15:27:10 +0100

    ab ... zy

---
+++ exercises/070/solution.py
@@ -0,0 +1,6 @@
+a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+a += ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+for i in a:
+    for j in a:
+        if i != j:
+            print(i + j)
