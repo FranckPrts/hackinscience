9af74ac6becb32da21f7f6026cf0e20e060185b9
fporte <franck.porteous@cri-paris.org>
2015-12-04 15:46:48 +0100

    le premier feu

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -5,7 +5,7 @@ def sort_a_list(l):
     a = sorted(l, reverse=True)
     return(a)
 
-    
+
 def sort_by_mark(my_class):
     a = sorted(my_class, key=operator.itemgetter(0), reverse=True)
     return(a)
